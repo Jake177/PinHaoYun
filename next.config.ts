617e12be91e316be10126656e3cef19fe5b17fac
Jake177 <lingjianhui177@gmail.com>
2025-12-29 00:30:29 +1100

    feat: Add S3 bucket and videos table environment variables in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,9 @@ const nextConfig: NextConfig = {
     COGNITO_USER_POOL_ID: process.env.COGNITO_USER_POOL_ID,
     COGNITO_CLIENT_ID: process.env.COGNITO_CLIENT_ID,
     COGNITO_CLIENT_SECRET: process.env.COGNITO_CLIENT_SECRET,
+    VIDEOS_TABLE: process.env.VIDEOS_TABLE,
+    S3_ORIGINAL_BUCKET: process.env.S3_ORIGINAL_BUCKET,
+    S3_THUMBNAIL_BUCKET: process.env.S3_THUMBNAIL_BUCKET,
   },
 };
 
