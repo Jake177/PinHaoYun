37bc8d54385ce34b61c8bb40b6aab4bf34e36d01
Jake177 <lingjianhui177@gmail.com>
2025-12-29 00:23:13 +1100

    feat: Add environment variables for Cognito configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   typedRoutes: true,
+  env: {
+    COGNITO_REGION: process.env.COGNITO_REGION,
+    COGNITO_USER_POOL_ID: process.env.COGNITO_USER_POOL_ID,
+    COGNITO_CLIENT_ID: process.env.COGNITO_CLIENT_ID,
+    COGNITO_CLIENT_SECRET: process.env.COGNITO_CLIENT_SECRET,
+  },
 };
 
 export default nextConfig;
